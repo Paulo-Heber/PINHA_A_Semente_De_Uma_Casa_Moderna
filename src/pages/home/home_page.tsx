6ef35445999e0ec589fc56343b6f880075de3cc4
Paulo Heber <pauloheber2017@gmail.com>
2024-04-02 21:32:34 -0300

    alteração do tipo de arquivo do head, da maneira de exportação e importação na HomePage

--- src/pages/home/home_page.tsx
+++ src/pages/home/home_page.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import { Head } from "../../components/head.tsx";
 
 export function HomePage() {
     return (
         <>
+            <Head />
             <h1>Essa é a página inicial</h1>
             <Link to="/log_page" />
             <Link to="/environments" />
