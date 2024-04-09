e133f82d3cf19a802527d0aa102a9e9f9a45a525
neemias-abner <neemiasabner.br@gmail.com>
2024-04-09 19:53:30 -0300

    commit

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,7 +6,7 @@ import logoImg from "../../img/logo.png";
 
 import styled from 'styled-components';
 
-import { faShoppingCart, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faShoppingCart, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export const Header: React.FC = () => {
@@ -18,7 +18,6 @@ export const Header: React.FC = () => {
   };
 
   return (
-
     <Head>
       <Logo>
         <Badge src={logoImg} alt="logo" />
@@ -28,49 +27,76 @@ export const Header: React.FC = () => {
         </div>
       </Logo>
       <Searce>
-        <SrcInput value={pesquisa} onChange={handlePesquisaChange} placeholder="Ex: Cama..." />
+        <SrcInput value={pesquisa} onChange={handlePesquisaChange} placeholder='Ex: Casa' />
+        <button><FontAwesomeIcon icon={faSearch} /></button>
       </Searce>
       <UserSpace>
         <FontAwesomeIcon icon={faShoppingCart} />
-        <FontAwesomeIcon icon={faUserCircle} />
-
-
+        <UserLog><FontAwesomeIcon icon={faUserCircle} /></UserLog>
       </UserSpace>
     </Head>
-
   );
 };
 
 const Head = styled.header`
 background-color:#8B4513;
-justify-content: space-between;
 display:flex;
+justify-content: space-between;
 width: 100%;
 padding:10px;
-`
-const Badge = styled.img`
-
-background-color:#fff000;
-max-block-size: 50px;
-
 `
 const Logo = styled.div`
 display:flex;
 color:#ffff;
 align-items: center;
 background-color:#151515;
-border: 1px;
-border-radius:10px;
-padding:10px;
+border: 3px solid #8B4513;
+border-radius:15px;
+width: 340px;
+height:105px;
+padding:20px;
 margin:10px;
+box-shadow:0px 0px 0px 3px #151515;
+`
+const Badge = styled.img`
+max-block-size: 50px;
+margin-right:15px;
 `
 const Searce = styled.div`
 display:flex;
 align-items: center;
+position: relative;
+&> input {
+  width:400px;
+  height:40px;
+  display:flex;
+  border-radius:40px;
+  padding:10px;
+  border:none;
+}
+&> button {
+  position:absolute;
+  right:10px;
+  border:none;
+  background:transparent;
+  
+}
 
 `
 const UserSpace = styled.div`
 display:flex;
 align-items: center;
+&>*{
+  font-size: 40px ;
+  color:#ffff;
+  padding-right:30px;
+  border:none;
+  background:transparent;
+}
+`
+const UserLog = styled.button`
 
+&>*{
+  
+}
 `
