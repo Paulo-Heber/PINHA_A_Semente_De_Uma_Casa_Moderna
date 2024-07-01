58214bdb5f366e710b2fc61760c94c3618cf6003
Paulo Heber <pauloheber2017@gmail.com>
2024-07-01 17:59:28 -0300

    Adicionando estilo de movimentação com o botão do carrocel

--- src/components/products-cards/product-card-style.tsx
+++ src/components/products-cards/product-card-style.tsx
@@ -29,11 +29,26 @@ export const FeaturedProductInformation = styled.div`
     letter-spacing:.1rem;
     flex-direction:column;
 
+    & > :nth-child(2){
+    margin-block: .5rem;
+    overflow: hidden;
+    }
+
     & > :last-child{
     background-color:${colors.secondaryColor};
     border:none;
     color:#ffffff;
     padding:1rem;
     width:50%;
+    transition:.1s ease-in-out;
+    }
+
+    & > :last-child:hover{
+    background-color:#333333;
+    cursor:pointer;
+    }
+
+     & > :last-child:active {
+     box-shadow: inset 0px 2.5px 0px 0px #4c4c4c; 
     }
     `
